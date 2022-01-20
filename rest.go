eee397f723bc204afce09a4271b70a58c1083096
NahiomyJanse <nahiomy.janse@gmail.com>
2022-01-19 20:20:53 -0500

    mensaje_de_mis_cambios

--- rest.go
+++ rest.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	opcion_menu_principal := 0
+
 	pasta_boloñesa := 30
 	ensalada_italiana := 20
 	pizza_hawaiana := 10
@@ -29,60 +29,80 @@ func main() {
 		8. Ensalada tradicional`
 
 	for {
-		if opcion_menu_principal == 0 {
-			fmt.Println(menu_principal)
-			fmt.Println("¿Qué desea ordenar?")
-			_, err := fmt.Scanf("%d", &orden)
+
+		fmt.Println(menu_principal)
+		fmt.Println("¿Qué desea ordenar?")
+		_, err := fmt.Scanf("%d", &orden)
+		if err != nil {
+			panic(err)
+		}
+		if orden == 1 {
+			valor_total = valor_total + pasta_boloñesa
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
 			if err != nil {
 				panic(err)
 			}
-			if orden == 1 {
-				valor_total = valor_total + pasta_boloñesa
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 2 {
+			valor_total = valor_total + ensalada_italiana
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 2 {
-				valor_total = valor_total + ensalada_italiana
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 3 {
+			valor_total = valor_total + pizza_hawaiana
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 3 {
-				valor_total = valor_total + pizza_hawaiana
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 4 {
+			valor_total = valor_total + pizza_vegana
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 4 {
-				valor_total = valor_total + pizza_vegana
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 5 {
+			valor_total = valor_total + pasta_marinera
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 5 {
-				valor_total = valor_total + pasta_marinera
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 6 {
+			valor_total = valor_total + sandwich_tres_quesos
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 6 {
-				valor_total = valor_total + sandwich_tres_quesos
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 7 {
+			valor_total = valor_total + sandwich_vegetariano
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 7 {
-				valor_total = valor_total + sandwich_vegetariano
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
+		}
+		if orden == 8 {
+			valor_total = valor_total + ensalada_tradicional
+			fmt.Println("¿Desea seguir ordenando?")
+			_, err := fmt.Scanf("%s", &seguir)
+			if err != nil {
+				panic(err)
 			}
-			if orden == 8 {
-				valor_total = valor_total + ensalada_tradicional
-				fmt.Println("¿Desea seguir ordenando?")
-				fmt.Scanf(seguir)
-			}
-			if seguir == "SI" {
-				opcion_menu_principal = 0
-			} else if seguir == "" {
-				fmt.Println("El valor a pagar es de $", valor_total)
-				break
-			}
-
+		}
+		if seguir != "SI" {
+			fmt.Println("El valor a pagar es de $", valor_total)
+			break
 		}
 
 	}
